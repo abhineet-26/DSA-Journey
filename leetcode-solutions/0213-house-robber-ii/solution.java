f5fc56640ec31d4993185a1625a80e00c50e1766
Abhineet <194663976+abhineet-26@users.noreply.github.com>
2026-02-23 10:40:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (40.70%)

---
+++ leetcode-solutions/0213-house-robber-ii/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        return Math.max(robLinear(nums, 0, n - 2),
+                        robLinear(nums, 1, n - 1));
+    }
+
+    private int robLinear(int[] nums, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+        for (int i = start; i <= end; i++) {
+            int pick = nums[i] + prev2;
+            int skip = prev1;
+            int cur = Math.max(pick, skip);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return prev1;
+    }
+}
+
