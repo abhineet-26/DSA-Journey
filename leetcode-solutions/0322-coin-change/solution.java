700b96718e9b584ac12b28ef381539a0cae68fe2
Abhineet <194663976+abhineet-26@users.noreply.github.com>
2026-02-23 10:38:29 +0000

    Sync LeetCode submission Runtime - 10 ms (99.79%), Memory - 46.3 MB (71.38%)

---
+++ leetcode-solutions/0322-coin-change/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i <= amount; i++) dp[i] = max;
+
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int a = coin; a <= amount; a++) {
+                dp[a] = Math.min(dp[a], dp[a - coin] + 1);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
+
